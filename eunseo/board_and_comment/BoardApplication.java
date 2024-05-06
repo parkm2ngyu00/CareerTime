6731e5d0ffef2ec119e4bfa5d353202ba9b7a09b
이은서 <163079918+Cutemaru@users.noreply.github.com>
2024-05-06 23:20:46 +0900

    Add files via upload

---
+++ eunseo/board_and_comment/BoardApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.dankook.ace.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 스프링 부트의 주요 설정과 애플리케이션을 자동 설정
+public class BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args); // 스프링 애플리케이션을 실행
+    }
+}
