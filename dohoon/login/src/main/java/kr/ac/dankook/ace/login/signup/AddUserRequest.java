10e963173961e41a825d407baff6658f653a3e97
isasgp <isasgp@gmail.com>
2024-05-06 22:40:14 +0900

    login and signup

---
+++ dohoon/login/src/main/java/kr/ac/dankook/ace/login/signup/AddUserRequest.java
@@ -0,0 +1,11 @@
+package kr.ac.dankook.ace.login.signup;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
---
+++ dohoon/login/src/main/java/kr/ac/dankook/ace/login/user/UserDetailService.java
@@ -0,0 +1,18 @@
+package kr.ac.dankook.ace.login.user;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
