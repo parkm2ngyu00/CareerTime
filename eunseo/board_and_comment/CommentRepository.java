09ea63561f800d88b9c6e511bd5117b33f5428b9
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2024-06-16 10:08:26 +0900

    디렉토리 정리

--- eunseo/board_and_comment/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.dankook.ace.board.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- mingyu/backend/careertime/src/main/java/kr/ac/dankook/ace/careertime/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.dankook.ace.careertime.repository;
-
-import kr.ac.dankook.ace.careertime.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
